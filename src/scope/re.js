6ea9b9475f50b38a7bc2a6a51a4d4741e442fb03
Hector Santana <[hector.santana.gc@gmail.com]>
2022-07-17 14:54:37 +0100

    subir practicas curso

---
+++ src/scope/re.js
@@ -0,0 +1,32 @@
+var firsName; // undefined
+firsName = 'Oscar';
+console.log(firsName);
+
+var lastName = 'David'; // declarar / asignar
+lastName = 'Ana'; // reasignar
+console.log(lastName);
+
+var secondName = 'David'; // declarando / asignando
+var secondName = 'Ana'; // reasignando
+console.log(secondName);
+
+// let
+
+let fruit = 'Apple' // declarar y asignar
+fruit = 'Kiwi'; // reasignar
+console.log(fruit);
+
+let fruit = 'Banana'; // No se puede redeclarar porque ya existe
+console.log(fruit);
+
+// const
+const animal = 'dog' // declara y asigna
+animal = 'cat'; // reasignando
+console.log(animal); // Da error porque no podemos reasignar
+
+const vehicles = [];
+vehicles.push("car");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
